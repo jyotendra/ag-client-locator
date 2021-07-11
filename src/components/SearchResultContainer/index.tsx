43b077f90892f574f0c9850b88178a8f962d23ff
jyotendra sharma <jyotendrasharma92@gmail.com>
2021-07-11 15:32:12 +0530

    Created Search result comp (#13)
    * added component skeleton
    
    * completed searchBar, loader and theme
    
    * added noData component
    
    * created searchResultComponent; yet to justify
    
    * added place holder component
    
    * added searchResultContainer

--- src/components/SearchResultContainer/index.tsx
+++ src/components/SearchResultContainer/index.tsx
@@ -1,3 +1,37 @@
-const SearchResultContainer = () => <div>SearchResultContainer</div>;
+import { Grid, makeStyles, Paper } from "@material-ui/core";
+import { yelpResponseType } from "../../shared/types/yelp-response.type";
+import SearchResult from "../SearchResult";
+
+const useStyles = makeStyles({
+  scrollableDiv: {
+    overflowY: "scroll",
+    height: "75vh",
+  },
+  content: {
+    marginBottom: "1rem",
+    marginTop: "1rem",
+  },
+});
+
+const SearchResultContainer = ({ searchResults }: searchResultProps) => {
+  const classes = useStyles();
+  return (
+    <Paper elevation={2}>
+      <div className={classes.scrollableDiv}>
+        <Grid container justifyContent="space-evenly">
+          {searchResults.businesses.map((b, indx) => (
+            <Grid key={`res-${indx}`} item className={classes.content}>
+              <SearchResult placeInfo={b} />
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+    </Paper>
+  );
+};
+
+type searchResultProps = {
+  searchResults: yelpResponseType;
+};
 
 export default SearchResultContainer;
